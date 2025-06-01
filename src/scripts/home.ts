db5202b6b78dcebb5225fbd81cc7cd542c4c1a02
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2025-06-01 15:42:48 +0000

    Add animate hero and paragraph in Home page

--- src/scripts/home.ts
+++ src/scripts/home.ts
@@ -1,2 +1,67 @@
+import { safeExecute } from '@utils/helpers';
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+
+function initGsap() {
+  gsap.registerPlugin(SplitText, ScrollTrigger);
+
+  ScrollTrigger.defaults({
+    scrub: 1,
+    toggleActions: 'play none none reverse',
+  });
+}
+
+function animateHeroContent(className: string) {
+  const content = document.querySelector(className);
+  if (!content) return;
+
+  gsap.to(content, {
+    scrollTrigger: {
+      trigger: content,
+      start: 'top 20%',
+    },
+    opacity: 0,
+    scale: 0,
+    ease: 'none',
+    onComplete: () => {
+      ScrollTrigger.refresh();
+    },
+  });
+}
+
+function animateRevealParagraph(className: string) {
+  const paragraphs = document.querySelectorAll(className);
+  if (!paragraphs?.length) return;
+
+  paragraphs.forEach((paragraph) => {
+    const splitText = new SplitText(paragraph, { type: 'words' });
+    const { words } = splitText;
+
+    const COLORS = {
+      from: 'var(--text-color--text-alternate)',
+      to: 'var(--text-color--text-secondary)',
+    };
+
+    gsap.set(words, { color: COLORS.from });
+    gsap.set(paragraph, { color: COLORS.from });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: paragraph,
+        start: 'top 50%',
+        end: 'bottom 20%',
+      },
+    });
+
+    words.forEach((word) => {
+      tl.to(word, { color: COLORS.to, duration: 0 }, '<0.1');
+    });
+  });
+}
+
 window.Webflow ||= [];
-window.Webflow.push(() => {});
+window.Webflow.push(() => {
+  safeExecute(initGsap);
+  safeExecute(animateHeroContent, '.hero_content');
+  safeExecute(animateRevealParagraph, '.intro_content p');
+});
